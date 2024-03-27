64c7e5f710daaf8367c2c49c12c16c94f8b9308e
JustFiesta <mateusz.bocak9@gmail.com>
2024-03-27 21:53:45 +0100

    lab3

---
+++ NativeJavaGraphics/src/main/java/labs/no3/Vec3i.java
@@ -0,0 +1,18 @@
+package labs.no3;
+
+public class Vec3i {
+    public int x;
+    public int y;
+    public int z;
+
+    @Override
+    public String toString() {
+        return x + " " + y + " " + z;
+    }
+
+    public Vec3i(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
